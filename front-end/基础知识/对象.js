cbda46b198cb5e8d5233c5547da29ccdc67c83e5
wgf <wgf_1991@163.com>
2021-05-20 22:22:18 +0800

    20210520

--- front-end/基础知识/对象.js
+++ front-end/基础知识/对象.js
@@ -18,7 +18,8 @@ console.log('-------------------------------------------------------------------
  * 设置属性标志
  * 函数： Object.defineProperty(object, propertyName, descriptor)
  */
-//设置value： 修改内容
+//设置value： 修改内容 
+//通过value设置的新属性，如果不设置writable、enumerable、configurable，则会被设置为false
 Object.defineProperty(objProperty, 'name', {
     value: 'setName'
 });
@@ -31,8 +32,24 @@ console.log('新增属性newName并赋值newName，赋值结果为：', objPrope
 console.log('----------------------------------------------------------------------------------------------------');
 //修改只读属性false(设置为只读)。严格模式下，赋值会出现error，非严格模式下，编译器会忽略赋值
 Object.defineProperty(objProperty, 'name', {
-    wirtable: false
+    writable: false
 });
-// objProperty.name = 'writable' // error
-console.log('设置只读属性之后，严格模式下，赋值会出现error，普通模式下，编译器会忽略赋值。objProperty.name=', objProperty.name);
+console.log(`设置只读属性标志后，name的属性标志为：`,Object.getOwnPropertyDescriptor(objProperty,'name'));
+Object.getOwnPropertyDescriptor(objProperty, 'name')
+objProperty.name = 'writable' // error
+console.log('设置只读属性之后，严格模式下，赋值会出现error，普通模式下，编译器会忽略赋值。objProperty.name=', objProperty.name, );
 console.log('----------------------------------------------------------------------------------------------------');
+//设置为不可迭代： enumerable。
+//属性newName不存在是因为newName是通过Object.defineProperty的value属性添加。
+//并且添加的时候，没有设置wirtable、enumerable、configurable属性，所以这三个属性的默认值为false
+console.log(`设置前打印,可迭代属性为：${Object.keys(objProperty)}`);
+console.log(Object.getOwnPropertyDescriptor(objProperty, 'newName'));
+console.log('----------------------------------------------------------------------------------------------------');
+//删除不可删除属性
+//newName的属性标志configurable是false
+console.log(`输出newName的属性标志：`,Object.getOwnPropertyDescriptor(objProperty, 'newName'));
+console.log(`输出name的属性标志：`,Object.getOwnPropertyDescriptor(objProperty, 'name'));
+delete objProperty.newName;
+console.log(`执行delete objProperty.newName之后，输出newName的值${objProperty.newName},可以看出删除失败。`);
+// objProperty.newName = 'test'
+// objProperty.name = 'test1'
